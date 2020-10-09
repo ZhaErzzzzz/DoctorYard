e0fbd9c045fc3aa6af005254d324b962bc9b1ad4
zyq <1351310082@qq.com>
2020-10-09 10:07:58 +0800

    修改了dubbo报错问题，以及热部署的问题  ---第5次  from 赵昱钦

--- doctoryard-controller/src/main/java/com/four/security/token/SecurityConfig.java
+++ doctoryard-controller/src/main/java/com/four/security/token/SecurityConfig.java
@@ -52,6 +52,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/js/**", "/css/**", "/images/**");
+        web.ignoring().antMatchers("/js/**", "/css/**", "/images/**,/font_Icon/**,/fonts/**","/static/**");
     }
 }
--- doctoryard-service/src/main/java/com/four/DoctoryardServiceApplication.java
+++ doctoryard-service/src/main/java/com/four/DoctoryardServiceApplication.java
@@ -1,9 +1,13 @@
 package com.four;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.four")
+@EnableDubbo //开启dubbo的注解支持
 public class DoctoryardServiceApplication {
 
     public static void main(String[] args) {
